e0611adf38f709fe742288fb06910032562d59df
Eduardo Bonilla <ebonilla88@gmail.com>
2019-12-04 19:47:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,9 @@ def expect(first_number, second_number)
 end
 
 def expect(first_number, second_number)
+  return first_number - second_number 
+end
+
+def expect(first_number, second_number)
   return first_number * second_number 
 end
