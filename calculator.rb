d6dd6b9c3d8462437ab10c234279ccb6b55a10cd
Eduardo Bonilla <ebonilla88@gmail.com>
2019-12-04 20:15:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,17 +1,2 @@
-expect(second_number).not_to equal (0)
-
-def expect(first_number, second_number)
-  return first_number + second_number 
-end
-
-def expect(first_number, second_number)
-  return first_number - second_number 
-end
-
-def expect(first_number, second_number)
-  return first_number * second_number 
-end
-
-def expect(first_number, second_number)
-  return first_number / second_number 
-end
+first_number = 5
+second_number = 5
